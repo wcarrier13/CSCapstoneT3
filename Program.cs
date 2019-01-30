dccff8feff50dc48f39fcd085359471276f9412e
Cody <codyanderson@unomaha.edu>
2019-01-30 02:19:32 +0000

    MVC Template

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Lizst
+namespace LizstMVC
 {
     public class Program
     {
