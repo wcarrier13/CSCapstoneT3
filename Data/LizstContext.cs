ae03d497baea9efef6c25427874551c4179d1ae1
Cody <codyanderson@unomaha.edu>
2019-02-08 19:02:26 -0600

    Configured the server to connect to MySQL database. Includes a test class for diagnosing.

---
+++ Data/LizstContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+using System.Configuration;
+
+namespace Lizst.Models
+{
+    public class LizstContext : DbContext
+    {
+        public LizstContext (DbContextOptions<LizstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lizst.Models.Test> Test { get; set; }
+    }
+}
