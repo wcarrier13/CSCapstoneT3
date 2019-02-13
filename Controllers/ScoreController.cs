836c28e4e697f4a85e37626d159f1c74fbbe1d2a
Rachael Neff <rachaeleneff@yahoo.com>
2019-02-13 15:20:45 -0600

    Score front end no backend hookups yet :(

--- Controllers/ScoreController.cs
+++ Controllers/ScoreController.cs
@@ -25,6 +25,16 @@ namespace Lizst.Controllers
             return View(await _context.Score.ToListAsync());
         }
 
+        public IActionResult AddScore()
+        {
+            return View();
+        }
+
+        public IActionResult EditEnsemble()
+        {
+            return View();
+        }
+
         // GET: Scores/Details/5
         public async Task<IActionResult> Details(int? id)
         {
