14e41d923e2673245c16d31a7fbbc8f853008f3f
Cody <codyanderson@unomaha.edu>
2019-02-18 18:38:35 -0600

    Begin process of updating score controller. Add comments as well.

--- Controllers/ScoreController.cs
+++ Controllers/ScoreController.cs
@@ -19,55 +19,25 @@ namespace Lizst.Controllers
             _context = context;
         }
 
-        // GET: Scores
+        // GET: Score
+        // Index page merely displays all scores.
         public async Task<IActionResult> Index()
         {
             return View(await _context.Score.ToListAsync());
         }
 
+        // GET: Score/AddScore
+        // Blank template for the addition of a new score.
         public IActionResult AddScore()
         {
             return View();
         }
-        public IActionResult EditScore()
-        {
-            return View();
-        }
-            public IActionResult EditEnsemble()
-        {
-            return View();
-        }
 
-        // GET: Scores/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var score = await _context.Score
-                .FirstOrDefaultAsync(m => m.ScoreId == id);
-            if (score == null)
-            {
-                return NotFound();
-            }
-
-            return View(score);
-        }
-
-        // GET: Scores/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Scores/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Score/AddScore
+        // New score is posted, add it to database.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Score score)
+        public async Task<IActionResult> AddScore(Score score)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +48,9 @@ namespace Lizst.Controllers
             return View(score);
         }
 
-        public async Task<IActionResult> Edit(int? id)
+        // GET: Score/EditScore
+        // If score is in database, give edit page with relevant details.
+        public async Task<IActionResult> EditScore(int? id)
         {
             if (id == null)
             {
@@ -94,11 +66,10 @@ namespace Lizst.Controllers
         }
 
         // POST: Tests/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Revised version of score has been posted. Update the database.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ScoreId", "Title", "Composer", "Genre", "NumberOfParts")] Score score)
+        public async Task<IActionResult> EditScore(int id, [Bind("ScoreId", "Title", "Composer", "Genre", "NumberOfParts")] Score score)
         {
             if (id != score.ScoreId)
             {
@@ -126,35 +97,6 @@ namespace Lizst.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(score);
-        }
-
-        // GET: Scores/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var score = await _context.Score
-                .FirstOrDefaultAsync(m => m.ScoreId == id);
-            if (score == null)
-            {
-                return NotFound();
-            }
-
-            return View(score);
-        }
-
-        // POST: Scores/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var score = await _context.Score.FindAsync(id);
-            _context.Score.Remove(score);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ScoreExists(int id)
