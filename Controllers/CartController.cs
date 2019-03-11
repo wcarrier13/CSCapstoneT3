142e97ff8e9a3802609c922180d4f97258e41ea7
Cody <codyanderson@unomaha.edu>
2019-03-11 11:14:34 -0500

    Add/Remove in cart controller.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lizst.Models;
+
+namespace Lizst.Controllers
+{
+    public class CartController : Controller
+    {
+        LizstContext _context;
+        ICollection<Score> ShoppingCart;
+        public CartController(LizstContext context)
+        {
+            _context = context;
+            ShoppingCart = Cart.GetCart();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(ShoppingCart);
+        }
+
+        public IActionResult AddToCart(int id)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n" + id + "\n\n");
+            Score score = _context.Score.Find(id);
+            if(score == null)
+            {
+                System.Diagnostics.Debug.WriteLine("Not Found");
+                return NotFound();
+            }
+            System.Diagnostics.Debug.WriteLine(score.Title);
+            ShoppingCart.Add(score);
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveFromCart(int id)
+        {
+            Score score = ShoppingCart.Single(s => s.ScoreId == id);
+            if(score == null)
+            {
+                return NotFound();
+            }
+            ShoppingCart.Remove(score);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace Lizst.Models
+{
+    public class Cart
+    {
+        public static ICollection<Score> ShoppingCart { get; } = new List<Score>();
+
+        public static ICollection<Score> GetCart()
+        {
+            return ShoppingCart;
+        }
+
+    }
+}
