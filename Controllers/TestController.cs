c0e0bf919b9c4c0613c0459f02045f31d17b5ccf
Cody <codyanderson@unomaha.edu>
2019-03-27 18:01:33 -0500

    Dynamically generated accordion example.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lizst.Models;
+
+namespace Lizst.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Test());
+        }
+    }
+}
---
+++ Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lizst.Models
+{
+    public class Test
+    {
+        public String[] Instruments = new string[] { "Flute","violin","chello" };
+        public int[] Nums = new int[] { 1, 2, 3 };
+    }
+}
