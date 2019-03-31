92a53b91b7b4ebbb7f2308a483256dff84950f70
Maria <mstathopoulos@unomaha.edu>
2019-03-31 12:24:24 -0500

    Making the pieces actually go into the database (right now everything is just called a flute and it's not connected to a score yet)

--- Controllers/ScorePiecesController.cs
+++ Controllers/ScorePiecesController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Lizst.Models;
 using Microsoft.AspNetCore.Mvc;
-using Lizst.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Lizst.Controllers
 {
@@ -22,15 +21,7 @@ namespace Lizst.Controllers
             return View(new ScorePieces());
         }
 
-        public async Task<IActionResult> AddTo(int p, int s)
-        {
-            ScorePieces piece = new ScorePieces { ScoreId = s, PieceId = p };
-            await _context.ScorePieces.AddAsync(piece);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index", new { id = s });
-        }
-
-        public IActionResult Submit()
+        public async Task<IActionResult> Submit()
         {
             List<String> test = new List<string>();
             //Finds every name for an input field. In the html we have <input ... name = "results[count]".../>
@@ -38,15 +29,49 @@ namespace Lizst.Controllers
 
             //Print everything for verification.
             System.Diagnostics.Debug.WriteLine("\n\n");
+            int count = 0;
+            string numberOfParts = "";
+            string edition = "";
+            string rating;
             foreach(String k in keys)
             {
+                if (count%3 == 0)
+                {
+                    
+                    numberOfParts = Request.Form[k];
+                }
+                if (count%3 == 1)
+                {
+                    edition = Request.Form[k];
+                }
+                if (count%3 == 2)
+                {
+                    rating = Request.Form[k];
+                    if (Convert.ToInt32(numberOfParts) > 0)
+                    {
+                        Piece piece = new Piece { Instrument = "Flute", NumberofParts = Convert.ToInt32(numberOfParts), Edition = edition };
+                        await _context.Piece.AddAsync(piece);
+                        await _context.SaveChangesAsync();
+                    }
+                }
+
                 //Key name (like results[1])
                 System.Diagnostics.Debug.WriteLine(k);
                 //Key value.
+                System.Diagnostics.Debug.WriteLine(Request.Form[k][0]);
                 System.Diagnostics.Debug.WriteLine(Request.Form[k]);
+                //System.Diagnostics.Debug.WriteLine(Request.Form[k][1]);
+                //System.Diagnostics.Debug.WriteLine(Request.Form[k][2]);
+                //if (pos == 0)
+                //{
+                    //Piece piece = new Piece { Instrument = "Flute", NumberofParts = Convert.ToInt32(Request.Form[k])};
+                //}
+                count++;
             }
             System.Diagnostics.Debug.WriteLine("\n\n");
 
+               
+     
             return RedirectToAction("Index", "Score");
         }
     }
